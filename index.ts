5dabcaf8725931b2d3cdda66bf8430b96ac5aae0
Siraj Brepotra <sirajbrepotra@gmail.com>
2018-08-14 17:36:06 +0100

    Added Test cases for extending and implementing classes

--- index.ts
+++ index.ts
@@ -5,6 +5,12 @@ interface ITestCase {
     name: string;
 }
 
+interface IExtensionClass extends ITestCase {
+    age: number;
+    gender: string;
+    profession?: string;
+}
+
 export class TestCase {
     public name: string;
     public city: string;
@@ -16,4 +22,36 @@ export class TestCase {
         return `${this.name} lives in ${this.city}`;
     }
 }
-                                
+
+// tslint:disable-next-line:max-classes-per-file
+export class ExtensionClass extends TestCase {
+    public gender: string;
+    public age: number;
+    public profession?: string;
+    constructor(options: IExtensionClass) {
+        super(options);
+        this.age = options.age;
+        this.gender = options.gender;
+        this.profession = options.profession;
+    }
+    public possibleProfession() {
+        if (this.profession !== undefined) {
+            return `and works as a ${this.profession}`;
+        } else {
+            return '';
+        }
+    }
+    public whatAge() {
+        return `${this.name} is ${this.age} year(s) old.`;
+    }
+    public whatGender() {
+        return `${this.name} is a ${this.gender}.`;
+    }
+    public whatProfession() {
+        return `${this.name} works as a ${this.profession}.`;
+    }
+    public whatAll() {
+        return `This is the information for ${this.name} who lives in ${this.city} and is ${this.age} years old.
+        They are a ${this.gender} ${this.possibleProfession()}`;
+    }
+}
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import 'mocha';
 import { hello } from '../index';
 import { TestCase } from '../index';
+import { ExtensionClass } from '../index';
 
 describe('Hello function', () => {
 
@@ -25,6 +26,64 @@ describe('Test Class', () => {
     describe ('Test Class . whatName', () => {
         it('should return the correct string', () => {
             expect (theTestCase.whatName()).to.equal('Siraj lives in London');
+        });
+    });
+    describe('Extension Class', () => {
+        const theExtensionCase = new ExtensionClass({
+            age: 20,
+            city: 'London',
+            gender: 'Male',
+            name: 'Siraj',
+        });
+        it ('should return the same age that is defined', () => {
+            expect(theExtensionCase.age).to.equal(20);
+        });
+        it ('should return the same gender that is defined', () => {
+            expect(theExtensionCase.gender).to.equal('Male');
+        });
+        it('should return the same city that is inputted', () => {
+            expect(theExtensionCase.city).to.equal('London');
+        });
+        it('should return the same name that is inputted', () => {
+            expect(theExtensionCase.name).to.equal('Siraj');
+        });
+        describe ('Sentences', () => {
+            it('should return full sentence for age', () => {
+                expect(theExtensionCase.whatAge()).to.equal('Siraj is 20 year(s) old.');
+            });
+            it('should return full sentence for gender', () => {
+                expect(theExtensionCase.whatGender()).to.equal('Siraj is a Male.');
+            });
+            it(`should return '' for undefined profession`, () => {
+                expect(theExtensionCase.possibleProfession()).to.equal('');
+            });
+        });
+    });
+    describe ('Extension Class', () => {
+        const theExtensionCase = new ExtensionClass({
+            age: 20,
+            city: 'London',
+            gender: 'Male',
+            name: 'Siraj',
+        });
+        it ('should return a full sentence from whatAll', () => {
+            // tslint:disable-next-line:max-line-length
+            expect(theExtensionCase.whatAll()).to.equal(`This is the information for Siraj who lives in London and is 20 years old.
+        They are a Male `);
+        });
+    });
+    describe ('Full Extension Class', () => {
+        const theExtensionCase = new ExtensionClass({
+            age: 20,
+            city: 'London',
+            gender: 'Male',
+            name: 'Siraj',
+            profession: 'Software Developer',
+        });
+        it ('should return a full sentence from whatAll', () => {
+            // tslint:disable-next-line:max-line-length
+            expect(theExtensionCase.whatAll()).to.equal(`This is the information for Siraj who lives in London and is 20 years old.
+        They are a Male and works as a Software Developer`);
         });
     });
 });
