4a48dba3045ddbdf6e9e2bf0f04c669df1abc949
Siraj Brepotra <sirajbrepotra@gmail.com>
2018-08-14 15:29:50 +0100

    Updated tests

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
 export const hello = () => 'Hello World!';
+
+interface ITestCase {
+    city: string;
+    name: string;
+}
+
+export class TestCase {
+    public name: string;
+    public city: string;
+    constructor(options: ITestCase) {
+        this.city = options.city;
+        this.name = options.name;
+        }
+    public whatName() {
+        return `${this.name} lives in ${this.city}`;
+    }
+}
+                                
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,11 +1,30 @@
 import { expect } from 'chai';
 import 'mocha';
 import { hello } from '../index';
+import { TestCase } from '../index';
 
 describe('Hello function', () => {
 
     it('should return Hello World!', () => {
         const result = hello();
         expect(result).to.equal('Hello World!');
+    });
+});
+
+describe('Test Class', () => {
+    const theTestCase = new TestCase({
+        city: 'London',
+        name: 'Siraj',
+        });
+    it('should return the same city that is inputted', () => {
+        expect(theTestCase.city).to.equal('London');
+    });
+    it('should return the same name that is inputted', () => {
+        expect(theTestCase.name).to.equal('Siraj');
+    });
+    describe ('Test Class . whatName', () => {
+        it('should return the correct string', () => {
+            expect (theTestCase.whatName()).to.equal('Siraj lives in London');
+        });
     });
 });
