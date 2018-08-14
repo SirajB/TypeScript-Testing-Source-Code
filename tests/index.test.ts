72bf9b5f64528a48ad1c912714fb42eb094a049a
Siraj Brepotra <sirajbrepotra@gmail.com>
2018-08-14 12:35:02 +0100

    TSLint fixes

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,12 +1,11 @@
-import { hello } from '../index';
 import { expect } from 'chai';
 import 'mocha';
+import { hello } from '../index';
 
 describe('Hello function', () => {
 
     it('should return Hello World!', () => {
         const result = hello();
         expect(result).to.equal('Hello World!');
-        
     });
 });
